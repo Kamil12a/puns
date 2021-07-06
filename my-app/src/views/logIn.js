1ba00907fdc19436ec4e4a3e2c4739f268d66872
Kamil-PLewka <kamilplewka12a@wp.pl>
2021-07-06 12:57:39 +0200

    added handling on error in loginPage

--- my-app/src/views/logIn.js
+++ my-app/src/views/logIn.js
@@ -1,21 +1,63 @@
 // import { useEffect, useState, useRef } from "react";
-// import { startDrawing } from "../components/drawing/startDrawing";
-// import { drawing } from "../components/drawing/draw";
-// import { finishDrawing } from "../components/drawing/finishDraw";
-// import "../styles/drawField.css";
-// import { ToolBar } from "../components/views/ToolBar";
-// import { rubbering } from "../components/drawing/rubbering";
-// import { Chat } from "../components/views/chat";
-// import { fetchData } from "../components/firebase/fetchPasswords";
-// import { Card } from "react-bootstrap";
-// import "bootstrap/dist/css/bootstrap.min.css";
-
+import fire from "../fire";
+import { Form, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/logIn.css";
+import { useRef, useState } from "react";
 export function LogIn() {
-
-
+  const emailRef = useRef(null);
+  const alertRef = useRef(null);
+  const [password, setPassword] = useState("");
+  const [showAlert, setAlert] = useState(false);
+  const handleSignup = (e) => {
+    e.preventDefault();
+    const email = emailRef.current.value;
+    fire
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((error) => {
+        setAlert(true);
+        let errorMessage = error.message;
+        alertRef.current.innerText = errorMessage
+      });
+  };
   return (
     <>
-        <p>LOGIN</p>
+      <section className="logInSection">
+        <div className="logInContainer">
+          <Form onSubmit={handleSignup}>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                ref={emailRef}
+                type="email"
+                placeholder="Enter email"
+              />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Group>
+            {showAlert && (
+              <div ref={alertRef} className="alert alert-danger" role="alert">
+                This is a danger alert—check it out!
+              </div>
+            )}
+            <Button variant="secondary">Switch to sign in</Button>
+            <Button variant="primary" type="submit">
+              Sign up
+            </Button>
+          </Form>
+        </div>
+      </section>
     </>
   );
 }
